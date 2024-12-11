ffb22a47fbb85718341aa5f3143f7097eceda6e1
KrishDave1 <Krish.Dave@iiitb.ac.in>
2024-12-10 22:15:13 -0600

    Upgraded version

--- PluginBackend/build-files.sh
+++ PluginBackend/build-files.sh
@@ -2,8 +2,8 @@
 set -e
 
 # Install dependencies
-python3.9 -m ensurepip --upgrade
-python3.9 -m pip install --upgrade pip setuptools wheel
+python3.10 -m ensurepip --upgrade
+python3.10 -m pip install --upgrade pip setuptools wheel
 apt-get update && apt-get install -y python3-distutils
 
 # Install project requirements

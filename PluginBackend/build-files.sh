5e8b3204c83e777daffaea9cd768eac09c6124c9
KrishDave1 <Krish.Dave@iiitb.ac.in>
2024-12-10 21:54:42 -0600

    Updated script

--- PluginBackend/build-files.sh
+++ PluginBackend/build-files.sh
@@ -1,4 +1,16 @@
-echo "BUILD START"
-python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput --clear
-echo "BUILD END"
+#!/bin/bash
+set -e
+
+# Install dependencies
+python3.9 -m ensurepip --upgrade
+python3.9 -m pip install --upgrade pip setuptools wheel
+apt-get update && apt-get install -y python3-distutils
+
+# Install project requirements
+pip install -r PluginBackend/requirements.txt
+
+# Collect static files
+python PluginBackend/manage.py collectstatic --noinput
+
+# Run database migrations
+python PluginBackend/manage.py migrate
